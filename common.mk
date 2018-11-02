ed9ac7fb454b0ef685176378047526f18cf98331
mukesh22584 <mks22584@gmail.com>
2018-11-02 08:18:01 +0530

    msm8998: Don't build SnapdragonCamera2
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -140,8 +140,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    SnapdragonCamera2
-
+    
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
